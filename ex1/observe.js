5426ca412949ef208dc7fa8acf900d795d692ac1
s <s@salvadorp.com>
2016-03-23 00:41:58 -0300

    initialize xprex

--- ex1/observe.js
+++
@@ -1,9 +0,0 @@
-// declaro function
-var o = function () {};
-// hookeo observer
-Object.observe(o, function(ob) {
-  // log all changes.
-  console.log(ob);
-});
-// freeze object.
-Object.freeze(o);
